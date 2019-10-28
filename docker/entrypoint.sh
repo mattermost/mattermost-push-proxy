5f787848090be7e56bb4275c84d5d1937991152f
jaydeland <jaydeland@gmail.com>
2019-10-28 16:13:38 -0400

    Updating entrypoint to use /bin/sh and to add libc6-compat to the base Docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 #Read environment variables
 APPLE_PUSH_CERT_PASSWORD=${APPLE_PUSH_CERT_PASSWORD:-}
 APPLE_PUSH_TOPIC=${APPLE_PUSH_TOPIC:-com.mattermost.Mattermost}
 ANDROID_API_KEY=${ANDROID_API_KEY:-}
 
-if [ -f /config/config.json ]; then
+if [[ -f /config/config.json ]]; then
   echo "Using existing config file"
 else
   echo "Configuring using environment variables"
   cp /mattermost-push-proxy/config/mattermost-push-proxy.json /config/config.json
   cat /config/config.json
-  if [ -f /certs/apple-push-cert.pem ]; then
+  if [[ -f /certs/apple-push-cert.pem ]]; then
     sed -i -e "s/ApplePushCertPrivate.*/ApplePushCertPrivate\":\"/certs\/apple-push-cert.pem\",/" /config/config.json
     sed -i -e "s/ApplePushCertPassword.*/ApplePushCertPassword\":\"\/certs\/apple-push-cert.pem\",/" /config/config.json
   fi
