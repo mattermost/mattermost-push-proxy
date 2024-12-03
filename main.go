e12e722d7a8715476dc3101e85e63825aafb9fd1
Domenico Rizzo <domenico.rizzo@gmail.com>
2024-12-03 17:31:13 +0100

    Enhanced logger initialization and configuration
    The main function has been updated to include a more robust logger initialization process. This includes error handling for the creation of a new logger, as well as its configuration. If no logging is defined, a default config (console output) is used. A separate function has been added to provide this default logging configuration. The server now also ensures that the logger is properly shut down when it's no longer needed.

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/mattermost/mattermost-push-proxy/internal/version"
 	"github.com/mattermost/mattermost-push-proxy/server"
+	"github.com/mattermost/mattermost/server/public/shared/mlog"
 )
 
 var (
@@ -39,7 +40,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	logger := server.NewLogger(cfg)
+	// Initialize the logger - begin
+	logger, err := mlog.NewLogger()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfgJSON := cfg.LoggingCfgJSON
+	if cfg.LoggingCfgFile == "" && cfgJSON == "" {
+		// if no logging defined, use default config (console output)
+		cfgJSON = defaultLoggingConfig()
+	}
+	err = logger.Configure(cfg.LoggingCfgFile, cfgJSON, nil)
+	if err != nil {
+		log.Fatal("Error in config file for logger: ", err)
+		return
+	}
+	defer func() { _ = logger.Shutdown() }()
+	// Initialize the logger - end
+
 	logger.Info("Loading " + fileName)
 
 	srv := server.New(cfg, logger)
@@ -52,4 +70,32 @@ func main() {
 	<-stopChan
 
 	srv.Stop()
+}
+
+func defaultLoggingConfig() string {
+	return `
+	{
+		"def": {
+			"type": "console",
+			"options": {
+				"out": "stdout"
+			},
+			"format": "plain",
+			"format_options": {
+				"delim": " ",
+				"min_level_len": 5,
+				"min_msg_len": 40,
+				"enable_color": true,
+				"enable_caller": true
+			},
+			"levels": [
+				{"id": 5, "name": "debug"},
+				{"id": 4, "name": "info", "color": 36},
+				{"id": 3, "name": "warn"},
+				{"id": 2, "name": "error", "color": 31},
+				{"id": 1, "name": "fatal", "stacktrace": true},
+				{"id": 0, "name": "panic", "stacktrace": true}
+			]
+		}
+	}`
 }
